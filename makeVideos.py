606ee27c4efc2c95c9184ca9527f411a1ac8209d
anugrah2002 <anugrahnigam16@gmail.com>
2021-03-03 00:15:50 +0530

    second commit

---
+++ makeVideos.py
@@ -0,0 +1,114 @@
+from downloader import *
+import os 
+import cv2 
+from PIL import Image 
+from django.conf import settings
+import urllib.request
+from gtts import gTTS,tts
+import shutil
+from moviepy.editor import AudioFileClip, VideoFileClip,CompositeAudioClip
+
+def makeAudio(name,content):
+    try:
+        os.chdir(os.path.join(settings.BASE_DIR, r"dataset/"+name))
+        ttsG = gTTS(content, lang='hi')
+        ttsG.save('audio.mp3')
+    except tts.gTTSError as e:
+        print(e)
+        return False
+    return True
+
+def downloadImages(title):
+    os.chdir(os.path.join(settings.BASE_DIR,''))
+    download(title, limit=10,  output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=300)
+
+def makeVideo(name,content):
+    print(os.path.isdir(os.path.join(settings.BASE_DIR, r"dataset/"+name)))
+    if os.path.isdir(os.path.join(settings.BASE_DIR, r"dataset/"+name)):
+        shutil.rmtree(os.path.join(settings.BASE_DIR, r"dataset/"+name))
+    downloadImages(name)
+    status = makeAudio(name,content)
+    if not status:
+        return 'GTTS ERR'
+       
+    print(os.getcwd()) 
+
+    os.chdir(os.path.join(settings.BASE_DIR, r"dataset/"+name)) 
+    path = os.path.join(settings.BASE_DIR, r"dataset/"+name)
+
+    mean_height = 720
+    mean_width = 1280
+
+    num_of_images = len(os.listdir('.')) 
+
+    # for file in os.listdir('.'): 
+    #     if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith("png"):
+    #         im = Image.open(os.path.join(path, file)) 
+    #         width, height = im.size 
+    #         mean_width += width 
+    #         mean_height += height 
+
+    # mean_width = int(mean_width / num_of_images) 
+    # mean_height = int(mean_height / num_of_images) 
+
+    for file in os.listdir('.'): 
+        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith("png"): 
+            # opening image using PIL Image 
+            im = Image.open(os.path.join(path, file)).convert('RGB')
+
+            # im.size includes the height and width of image 
+            width, height = im.size 
+            print(width, height) 
+
+            # resizing 
+            imResize = im.resize((mean_width, mean_height), Image.ANTIALIAS) 
+            imResize.save( file, 'JPEG', quality = 95) # setting quality 
+
+    generate_video(name) 
+
+    addAudioToVideo(name)
+    
+    return os.path.join(settings.BASE_DIR, r"dataset/"+name+r'/'+name+r'.mp4')
+
+
+# Video Generating function 
+def generate_video(name):
+    image_folder = '.' # make sure to use your folder 
+    video_name = 'mygeneratedvideo.mp4'
+    os.chdir(os.path.join(settings.BASE_DIR, r"dataset/"+name))
+    images = [img for img in os.listdir(image_folder) 
+    if img.endswith(".jpg") or img.endswith(".jpeg") or img.endswith("png")]
+ 
+
+    frame = cv2.imread(os.path.join(image_folder, images[0])) 
+
+    height, width, layers = frame.shape
+    frameRate=10
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), 1, (width, height)) 
+    audioLength = AudioFileClip('audio.mp3').duration
+
+    videoToLoop = audioLength
+    if videoToLoop <1:
+        videoToLoop = 1
+
+    # Appending the images to the video one by one 
+    while videoToLoop > 0:
+        for image in images:
+            for i in range(frameRate):
+                video.write(cv2.imread(os.path.join(image_folder, image)))
+        videoToLoop-=(frameRate*len(images))
+    # Deallocating memories taken for window creation 
+    #cv2.destroyAllWindows() 
+    video.release() # releasing the video generated 
+
+def addAudioToVideo(name):
+    os.chdir(os.path.join(settings.BASE_DIR, r"dataset/"+name))
+    audiofile = AudioFileClip('audio.mp3')
+    videoclip = VideoFileClip("mygeneratedvideo.mp4")
+    #new_audioclip = CompositeAudioClip([audiofile])
+    videoclip = videoclip.set_audio(audiofile)
+    # videoclip.audio = new_audioclip
+    videoclip = videoclip.subclip(0, audiofile.duration)
+    videoclip = videoclip.speedx(factor=1.3)
+    # videoclip = videoclip.fx(speedx, 1.3)
+    videoclip.write_videofile(name+".mp4")
---
+++ views.py
@@ -0,0 +1,51 @@
+import os 
+# from django.http import HttpResponse
+# from django.conf import settings
+from processArticle import *
+from makeVideos import *
+from wsgiref.util import FileWrapper
+from uploadToYT import *
+import os
+from django.conf import settings
+import shutil
+import datetime
+from gtts import gTTS
+import datetime,random
+
+
+def requestVideo(request):
+    # objTime =/ models.uploadedVideos.objects.latest('id')
+
+    # if objTime is None or objTime.nextRandom is None or objTime.nextRandom > datetime.datetime.now(datetime.timezone.utc):
+    #     return HttpResponse('Time Error')
+        
+    title,summary,content,YTtitle = findArticle()
+
+    title='google'
+
+    if title == 0:
+        return HttpResponse('No more Unique')
+
+
+    p = makeVideos.makeVideo(YTtitle[:240]+' hd',content)
+
+    if p =='GTTS ERR':
+        shutil.rmtree(os.path.join(settings.BASE_DIR, r"dataset"))
+        return HttpResponse('GTTS ERR')
+
+    os.chdir(os.path.join(settings.BASE_DIR,''))
+
+    credit = '''\nWe take DMCA very seriously. All the images are from Bing Images.Since all the contents are not moderated so If anyway we hurt anyone sentiment, send us an request with valid proof.
+    '''
+    keywords = ','.join(str(YTtitle).split())
+
+    print(YTtitle)
+
+
+    command = 'python uploadToYT.py --file="'+str(p)+'" --title="'+YTtitle+'" --description="'+(summary+'\n'+credit)+'" --keywords="'+keywords+',hour news,news" --category="25" --privacyStatus="public" --noauth_local_webserver '
+    os.system(command) #comment this to stop uploading to youtube
+    # shutil.rmtree(os.path.join(settings.BASE_DIR, r"dataset")) # comment this to stop removing the file from system
+    obj = models.uploadedVideos(title = title,
+    nextRandom=(datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=random.randrange(245, 350))))
+    obj.save()
+    return HttpResponse('Success')
