21e9d9c04e0c1c754c338fbff6240db2550889ea
mashuk999 <mashuk1998@gmail.com>
2021-03-12 17:20:20 +0530

    automatic push

--- makeVideos.py
+++ makeVideos.py
@@ -97,6 +97,7 @@ def generate_video(name):
         image_folder = '.' # make sure to use your folder 
         video_name = 'mygeneratedvideo.mp4'
         os.chdir(os.path.join(settings.BASE_DIR, r"dataset/"+name))
+        print(os.listdir())
         images = [img for img in os.listdir(image_folder) 
         if img.endswith(".jpg") or img.endswith(".jpeg") or img.endswith("png")]
     
@@ -121,6 +122,7 @@ def generate_video(name):
         # Deallocating memories taken for window creation 
         #cv2.destroyAllWindows() 
         video.release() # releasing the video generated 
+        print(os.listdir())
     except:
         print('m.v. generatevideo')
     
